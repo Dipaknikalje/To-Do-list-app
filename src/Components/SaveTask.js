b40958131041060a841f02edfdc7c2a01440e681
Dipaknikalje <dipaknikalje80@gmail.com>
2023-10-09 22:39:42 +0530

    first commit

---
+++ src/Components/SaveTask.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+const SaveTask = (key, initialValue) => {
+  const [savedTask, setSaveTask] = useState(() => {
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(savedTask));
+  }, [key, savedTask]);
+  return [savedTask, setSaveTask];
+};
+
+export default SaveTask;
